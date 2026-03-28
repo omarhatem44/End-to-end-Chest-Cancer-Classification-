bf43eceb2575140d4b4c5ec8a6b8f35a9459906a
omarhatem44 <omarhatemmoahemd@gmail.com>
2026-03-28 21:13:10 +0200

    cicd added

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 import setuptools
-
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
+from pathlib import Path
 
 __version__ = "0.0.0"
 
@@ -11,6 +8,12 @@ AUTHOR_USER_NAME = "omarhatem44"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "omarhatemmoahemd@gmail.com"
 
+this_directory = Path(__file__).parent
+readme_path = this_directory / "README.md"
+
+long_description = ""
+if readme_path.exists():
+    long_description = readme_path.read_text(encoding="utf-8")
 
 setuptools.setup(
     name=SRC_REPO,
@@ -19,7 +22,7 @@ setuptools.setup(
     author_email=AUTHOR_EMAIL,
     description="A small python package for CNN app",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
